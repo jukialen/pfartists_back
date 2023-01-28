f745147b8f0a5c64e243471c5bca79de7b0f0625
jukialen <piteru1910@yahoo.com>
2023-01-28 22:56:08 +0100

    Update schema and User.dto | Addiction new relation

--- backend/src/DTOs/user.dto.ts
+++ backend/src/DTOs/user.dto.ts
@@ -2,7 +2,7 @@ export class UserDto {
   id: string;
   username?: string;
   pseudonym: string;
-  email: string;
+  email?: string;
   description?: string;
   profilePhoto?: string;
   plan: string;
