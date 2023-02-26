d3a2404f53d85a061fbef8e3dbff9ada9b541707
jukialen <pbidny@gmail.com>
2023-02-26 12:27:53 +0100

    Addiction methods for cors | minor changes

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -15,6 +15,7 @@ async function bootstrap() {
   app.enableCors({
     origin: `${process.env.WEB_DOMAIN}`,
     allowedHeaders: ['content-type', ...supertokens.getAllCORSHeaders()],
+    methods: ['GET', 'PATCH', 'POST', 'DELETE'],
     credentials: true,
   });
   const options: SwaggerDocumentOptions = {
