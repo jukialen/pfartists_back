e0087cdc54b56ccf033077438f3f7896892433e6
jukialen <pbidny@gmail.com>
2023-08-03 12:34:17 +0200

    Addiction FilesDto

--- backend/src/DTOs/file.dto.ts
+++ backend/src/DTOs/file.dto.ts
@@ -5,6 +5,18 @@ export class FileDto {
   profileType: boolean;
   tags: Tags;
 }
+
+export class FilesDto {
+  fileId?: string;
+  userId: string;
+  name: string;
+  tags: Tags;
+  pseudonym: string;
+  profilePhoto: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
 export class SortType {
   order: Prisma.FilesOrderByWithRelationInput;
   whereElements?: Prisma.FilesWhereInput;
