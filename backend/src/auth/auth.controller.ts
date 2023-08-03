edc01a72bc67669531a0c8bdfdeb2a02d2f593d2
jukialen <pbidny@gmail.com>
2023-08-03 11:29:46 +0200

    Addiction: updateMail for auth

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,21 +1,28 @@
-import { Body, Controller, Patch, UseGuards } from '@nestjs/common';
-import { AuthGuard } from './auth.guard';
-import { Session } from './session.decorator';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import ses, { SessionContainer } from 'supertokens-node/recipe/session';
 import ThirdPartyEmailPassword from 'supertokens-node/recipe/thirdpartyemailpassword';
+import { Session } from './session.decorator';
+import { AuthGuard } from './auth.guard';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Patch('/change-password')
+  @Post('change-password')
   @UseGuards(new AuthGuard())
   async updatePassword(
     @Session() session: SessionContainer,
-    @Body('oldPassword') oldPassword: string,
-    @Body('newPassword') newPassword: string,
+    @Body('passwords') passwords: { oldPassword: string; newPassword: string },
   ) {
+    const { oldPassword, newPassword } = passwords;
     const userId = session?.getUserId();
     const { email } = await ThirdPartyEmailPassword.getUserById(userId);
 
@@ -28,11 +35,23 @@ export class AuthController {
     );
 
     if (isPasswordValid.status !== 'OK') {
-      return;
+      throw new BadRequestException('Incorrect login details');
     }
 
     await this.authService.updatePassword(userId, newPassword);
     await ses.revokeAllSessionsForUser(userId);
+    await session.revokeSession();
+  }
+
+  @Patch('change-email')
+  @UseGuards(new AuthGuard())
+  async updateEmail(
+    @Session() session: SessionContainer,
+    @Body('data') data: { user_id: string; newEmail: string },
+  ) {
+    const { user_id, newEmail } = data;
+    await this.authService.updateMail(user_id, newEmail);
+    await ses.revokeAllSessionsForUser(user_id);
     await session.revokeSession();
   }
 }
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import ThirdPartyEmailPassword from 'supertokens-node/recipe/thirdpartyemailpassword';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class AuthService {
+  constructor(private prisma: PrismaService) {}
+  async updateMail(user_id: string, newEmail: string) {
+    await this.prisma.emailpassword_users.update({
+      where: { user_id },
+      data: { email: newEmail },
+    });
+    await this.prisma.emailverification_tokens.update({
+      where: { user_id_email_token: { user_id, email: null, token: null } },
+      data: { email: newEmail },
+    });
+    await this.prisma.emailverification_verified_emails.update({
+      where: { user_id_email: { user_id, email: null } },
+      data: { email: newEmail },
+    });
+  }
   async updatePassword(userId: string, newPassword: string) {
     await ThirdPartyEmailPassword.updateEmailOrPassword({
       userId,
